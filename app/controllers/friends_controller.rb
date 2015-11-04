9740bcfbd5c0f4d62a06b8b0e482377c0ab2874b
kinglemuel <pro@kinglemuel.com>
2015-11-03 22:48:06 -0500

    complete friendships

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,11 @@
 class FriendsController < ApplicationController
 	def create
+		user = User.find(params[:connection_id])
+		current_user.accept_friendship(user)
+		respond_to do |format|
+			format.html { redirect_to current_user }
+			format.js
+		end
 	end
 
 	def destroy
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,16 @@
 class RequestsController < ApplicationController
 	def create
-		@user = User.find(params[:connection_id])
-		current_user.requests.create(connection_id: @user.id) 
+		user = User.find(params[:connection_id])
+		current_user.requests.create(connection_id: user.id) 
 		respond_to do |format|
-			format.html { redirect_to @user }
+			format.html { redirect_to user }
 			format.js
 		end
 	end
 
 	def destroy
-		@user = User.find(params[:connection_id])
-		current_user.cancel_request @user 
+		user = User.find(params[:connection_id])
+		current_user.cancel_request user 
 		respond_to do |format|
 			format.html { redirect_to current_user }
 			format.js 
