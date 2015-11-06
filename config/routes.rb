b43fed4b9a3ca60a00423d09e2a0f5a5193de899
Umer <myemailum14@gmail.com>
2015-11-05 19:12:00 -0500

    fixed css

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
+  get ':userId?page=(:params)'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
